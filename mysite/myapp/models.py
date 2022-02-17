f21eed474a42344f70dd600d7e6dfb588fe2d387
Shelley Wong <shelleywong@Shelleys-MacBook-Pro.local>
2022-02-16 22:10:55 -0800

    adds initial model

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class QuestionModel(models.Model):
+    question_text = models.CharField(max_length=280)
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . import models
+
 # Create your views here.
 def index(request, page=0):
     #return HttpResponse("Hello World")
     squares = [x**2 for x in range(1, 11, 1)]
     page_list = list(range(page*10, page*10+10, 1))
+    q_list = models.QuestionModel.objects.all()
     context = {
         "title": "CINS 465",
         "message": "Hello World!",
         "squares": squares,
         "page_list": page_list,
         "prev": page-1,
-        "next": page+1
+        "next": page+1,
+        "q_list": q_list
     }
     return render(request, 'index.html', context=context)
