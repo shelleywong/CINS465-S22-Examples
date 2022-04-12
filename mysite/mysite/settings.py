966615b81c578323a96d34aedddb883e9029be30
Shelley Wong <swong26@csuchico.edu>
2022-04-12 00:09:30 +0000

    postgres cloud database

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -75,9 +75,24 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'OPTIONS': {
+    #         'service': 'my_service',
+    #         'passfile': '.my_pgpass',
+    #     },
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': '34.68.108.72',
+        'PORT': '5432',
     }
 }
 
